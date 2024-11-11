6ee4873af85fc2432b07f7bb30287cf1cbb74d1b
iskanderyunu <izzyyunu@gmail.com>
2024-11-10 21:09:24 -0500

    Updated Push

--- script/script.js
+++ script/script.js
@@ -1,34 +1,51 @@
 function main() {
+  //Use getElementbyID
   const title = document.getElementById("heading");
   title.style.color = "red";
+  //Use querySelector
   const title2 = document.querySelector("#heading");
   const screen = document.querySelector(".mainScreen");
   const paragraphs = document.querySelectorAll("p");
-  paragraphs.forEach((para) => (para.style.color = "pink"));
+  //Iterate over collection of elements
+  paragraphs.forEach((para) => (para.style.color = "red"));
   /*function callBack(para) {
     para.style.color = pink
 }
 paragraphs.forEach(callBack) */
 
-  //creating new element
+  //Use createElement
   const newpara = document.createElement("p");
-  newpara.textContent = "This is a new paragraph";
+  newpara.textContent = "🧨BOOM!💥";
+  //Use appendChild
   document.body.appendChild(newpara);
 }
 function changeTitleColor() {
   const title = document.getElementById("heading");
-  title.style.color = "blue";
+
+  // Toggle the color-change class
+  title.classList.toggle("color-change");
 }
+
 function unhide() {
   const paragraphToHide = document.querySelector(".sneaky");
   paragraphToHide.removeAttribute("hidden");
 }
+//Use response to user interaction
+// Define an array of colors
+const colors = ["red", "blue", "magenta", "springgreen", "purple"];
+let colorIndex = 0; // Track the current color index
+
 function hover() {
   const changeColor = document.querySelector(".mouseHover");
-  changeColor.style.color = "red";
+
+  // Set the color based on the current index
+  changeColor.style.color = colors[colorIndex];
+
+  // Increment the color index, looping back to the start if necessary
+  colorIndex = (colorIndex + 1) % colors.length;
 }
 
-// Test
+// One type of Form/Input
 const myForm = document.getElementById("myForm");
 myForm.addEventListener("submit", onSubmit);
 function onSubmit(evt) {
